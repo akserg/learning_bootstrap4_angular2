0dc721c637312e871bdfcfae98c0bf338365d197
Sergey Akopkokhyants <akserg@gmail.com>
2016-05-22 21:27:43 +0200

    Added app.component.html

--- chapter_2/ecommerce-seed/app/app.component.ts
+++ chapter_2/ecommerce-seed/app/app.component.ts
@@ -5,18 +5,8 @@ import {bootstrap} from '@angular/platform-browser-dynamic';
 
 @Component({
   selector: 'my-app',
-  template: `
-<div class="container"> 
-  <div class="page-header"> 
-    <h2>Chapter 1 <small>Hello, World</small></h2> 
-  </div> 
-  <div class="jumbotron"> 
-    <h1>Hello, {{name || 'World'}}</h1> 
-    <input type="text" [(ngModel)]="name" class="form-control input-lg">
-  </div> 
-</div>
-`})
-export class AppComponent { 
-	name: string = 'World';
+  templateUrl: 'app/app.component.html'
+})
+export class AppComponent {
 }
 
