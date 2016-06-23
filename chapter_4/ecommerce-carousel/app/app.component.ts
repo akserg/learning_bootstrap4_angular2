0cc1148a7ac09ae3c4c8011f7c18b067c815bfb3
Sergey Akopkokhyants <akserg@gmail.com>
2016-06-23 08:22:04 +0200

    Updated Welcome Page

--- chapter_4/ecommerce-carousel/app/app.component.ts
+++ chapter_4/ecommerce-carousel/app/app.component.ts
@@ -1,12 +1,12 @@
-// Import the decorator class for Component
 import { Component } from '@angular/core';
-// Import the bootstrap method to load application component
 import {bootstrap} from '@angular/platform-browser-dynamic';
+
+import {NavbarComponent} from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
+  viewProviders: [NavbarComponent]
 })
-export class AppComponent {
-}
+export class AppComponent {}
 
