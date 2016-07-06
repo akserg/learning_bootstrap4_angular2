511106c7ac5f391cffdf4901dfb806d1a5a55b2d
Sergey Akopkokhyants <akserg@gmail.com>
2016-07-06 22:10:55 +0200

    Updated footer

--- chapter_5/ecommerce-router/app/app.component.ts
+++ chapter_5/ecommerce-router/app/app.component.ts
@@ -8,6 +8,8 @@ import {ROUTER_DIRECTIVES} from '@angular/router';
  * Components
  */
 import {NavbarComponent} from './shared/navbar/navbar.component';
+import {FooterComponent} from './shared/footer/footer.component';
+
 import {WelcomeComponent} from './welcome/welcome.component';
 import {ProductListComponent, ProductComponent} from './product/index';
 import {CartComponent} from './cart/cart.component';
@@ -16,7 +18,7 @@ import {CheckoutComponent} from './checkout/checkout.component';
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
-  directives: [ROUTER_DIRECTIVES, NavbarComponent],
+  directives: [ROUTER_DIRECTIVES, NavbarComponent, FooterComponent],
   precompile: [WelcomeComponent, ProductListComponent, ProductComponent, CartComponent, CheckoutComponent]
 })
 export class AppComponent {
