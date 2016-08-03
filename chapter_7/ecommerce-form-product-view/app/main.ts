a82a87bdb8d1b346e3b47650e48f5086e392aa40
Sergey Akopkokhyants <akserg@gmail.com>
2016-08-03 13:22:01 +0200

    Added Product View

---
+++ chapter_7/ecommerce-form-product-view/app/main.ts
@@ -0,0 +1,37 @@
+/*
+ * Angular Imports
+ */
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {provide} from '@angular/core';
+import {provideRouter, RouterConfig} from '@angular/router';
+
+/*
+ * Components
+ */
+import {AppComponent} from './app.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {ProductListComponent} from './product/product-list.component';
+
+/*
+ * Routes
+ */
+const routes: RouterConfig = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'products', component: ProductListComponent }
+];
+
+/*
+ * Routes Provider
+ */
+const APP_ROUTER_PROVIDER = [
+    // Installs our routes
+    provideRouter(routes)
+];
+
+/*
+ * Bootstrap out application
+ */
+bootstrap(AppComponent, [
+    APP_ROUTER_PROVIDER
+]);
