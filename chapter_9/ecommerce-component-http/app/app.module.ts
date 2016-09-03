9a8993a8b77b0f6380faf53766c37d9ef3d91128
Sergey Akopkokhyants <akserg@gmail.com>
2016-09-03 16:44:16 +0200

    Created CartModule

--- chapter_9/ecommerce-component-http/app/app.module.ts
+++ chapter_9/ecommerce-component-http/app/app.module.ts
@@ -8,12 +8,13 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AppComponent}  from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {FooterComponent} from './footer/footer.component';
+import {CartModule} from './cart/cart.module';
 
 import {WelcomeComponent} from './welcome/welcome.component';
 import {CategoryListComponent, CategoryTitlePipe, CategoryCardComponent, CategorySlideComponent} from './category/index';
 import {ProductListComponent, ProductViewComponent, ProductCardComponent, ProductSearchComponent, ProductGridComponent} from './product/index';
-import {CartMenuComponent} from './cart/cart-menu.component';
-import {CartViewComponent} from './cart/cart-view.component';
+// import {CartMenuComponent} from './cart/cart-menu.component';
+// import {CartViewComponent} from './cart/cart-view.component';
 import {CheckoutViewComponent} from './checkout/checkout-view.component';
 
 /*
@@ -29,10 +30,10 @@ import {CartService} from './cart/cart.service';
 import {routing}  from './app.routes';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, routing],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, routing, CartModule],
   declarations: [AppComponent, NavbarComponent, FooterComponent, WelcomeComponent, CategoryListComponent,
     ProductListComponent, ProductViewComponent, ProductCardComponent, ProductSearchComponent, ProductGridComponent, 
-    CartMenuComponent, CartViewComponent, CategoryCardComponent, CategorySlideComponent, CheckoutViewComponent,
+    /*CartMenuComponent, CartViewComponent,*/ CategoryCardComponent, CategorySlideComponent, CheckoutViewComponent,
     CategoryTitlePipe],
   providers: [CategoryService, ProductService, CartService],
   bootstrap: [AppComponent]
