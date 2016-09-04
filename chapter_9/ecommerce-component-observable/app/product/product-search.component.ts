1016999eea6515fd5406019d4f0496d93176cfe5
Sergey Akopkokhyants <akserg@gmail.com>
2016-09-04 20:16:33 +0200

    Changed name of folder

---
+++ chapter_9/ecommerce-component-observable/app/product/product-search.component.ts
@@ -0,0 +1,21 @@
+/*
+ * Angular Imports
+ */
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+    selector: 'db-product-search',
+    templateUrl: 'app/product/product-search.component.html'
+})
+export class ProductSearchComponent {
+
+    disabled: boolean = true;
+    
+    constructor(private router: Router) {}
+
+    searchProduct(value: string) {
+        this.router.navigate(['/products'], { queryParams: { search: value} });
+    }
+}
+
