f00cd8a9187b557de9d6cc08baac3b0167beb33a
Sergey Akopkokhyants <sergey@bitx.co>
2016-10-07 07:45:11 +0200

    Project ecommerce-grid-in-grid migrated to Angular 2.0.0

--- chapter_3/ecommerce-grid-in-grid/app/main.ts
+++ chapter_3/ecommerce-grid-in-grid/app/main.ts
@@ -1,6 +1,7 @@
-// Import boostrap method to load our application component
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-// Load App Component class
-import { AppComponent } from './app.component';
-// Bootstrap out application
-bootstrap(AppComponent);
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+
+platform.bootstrapModule(AppModule);
